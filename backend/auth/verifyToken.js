461a2e020cb8efc02ad301407bfaed01f78e8a4d
sathurshanath <sathurshanath2021@gmail.com>
2024-01-26 11:00:11 +0530

    Implement Authorization

---
+++ backend/auth/verifyToken.js
@@ -0,0 +1,56 @@
+import jwt from "jsonwebtoken";
+import User from "../models/UserSchema.js";
+import Doctor from "../models/DoctorSchema.js";
+
+export const authenticate = async (req, res, next) => {
+  //get token from header
+  const authToken = req.headers.authorization;
+
+  //check token is exist
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "No token, authorization denied" });
+  }
+
+  try {
+    const token = authToken.split(" ")[1];
+
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.id = decoded.id;
+    req.role = decoded.role;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token is expired" });
+    }
+
+    return res.status(401).json({ success: false, message: "Invalid Token" });
+  }
+};
+
+export const restrict = (roles) => async (req, res, next) => {
+  const userId = req.id;
+
+  let user;
+
+  const patient = await User.findById(userId);
+  const doctor = await Doctor.findById(userId);
+
+  if (patient) {
+    user = patient;
+  }
+  if (doctor) {
+    user = doctor;
+  }
+
+ 
+
+  if (!roles.includes(user.role)) {
+    return res.status(401).json({ success: false, message: "You're not authorized" });
+  }
+  
+
+  next();
+};
