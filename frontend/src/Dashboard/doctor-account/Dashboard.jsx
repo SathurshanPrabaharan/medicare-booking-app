3686ca18967eb5c6c34a66707c1ae5a431068d4b
sathurshanath <sathurshanath2021@gmail.com>
2024-02-14 05:18:08 +0530

    Implement Dashboard

---
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Loader from "../../Components/Loader/Loading";
+import Error from "../../Components/Error/Error";
+import UseGetProfile from "../../Hooks/useFetchData";
+import { BASE_URL } from "../../config";
+import Tabs from "./Tabs";
+
+const Dashboard = () => {
+  const { data, loading, error } = UseGetProfile(
+    `${BASE_URL}/doctors/profile/me`
+  );
+
+  const [tab, setTab] = useState("overview");
+
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loader />}
+        {error && !loading && <Error />}
+        {!loading && !error && (
+          <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+            <Tabs tab={tab} setTab={setTab} />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
